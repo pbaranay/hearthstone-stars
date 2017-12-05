c0903d59710fb7338247b7af541ff800d2818fc3
Paul Baranay <pbaranay@gmail.com>
2017-12-05 14:50:26 -0500

    Add comments

--- hearthstone_stars.py
+++ hearthstone_stars.py
@@ -1,3 +1,13 @@
+"""
+Hearthstone is a popular online collectible card game (CCG). One of its play modes, Ranked, features
+a monthly progression where users earn "stars" and advance up a ladder of "ranks," usually striving
+to reach the ultimate rank, Legend. Winning a game nets you a star, while losing a game usually
+results in losing a star. Winning several games in a row generally merits an extra star.
+
+This program simulates how many games a player needs to play in order to reach Legend, given a
+particular average win rate. Other configuration variables can be specified as well, although the
+defaults match Hearthstone's standard implementation.
+"""
 from collections import OrderedDict
 import math
 import random
@@ -5,15 +15,19 @@ import statistics
 
 
 def build_star_map():
+    """
+    The star map is a dictionary that translates how many total stars you have into a rank.
+    """
     # total number of stars: rank
     d = OrderedDict()
-    ranks = range(1, 26)
+    ranks = range(1, 26)  # The range [1, 25]; i.e. the numbers 1 to 25, inclusive.
     ranks.reverse()
     num_stars = 0
     stars_per_rank = 2
     d[0] = 25
     for current_rank in ranks:
-        if current_rank % 5 == 0 and current_rank not in [25,5]:
+        # At rank 20, 15, and 10, the number of stars per rank increases by 1.
+        if current_rank in [20, 15, 10]:
             stars_per_rank += 1
         for _ in xrange(stars_per_rank):
             num_stars += 1
@@ -38,6 +52,18 @@ def climb_to_legend(
         no_streaks_above_rank=5,
         rank_floors=DEFAULT_RANK_FLOORS
 ):
+    """
+    Use a random number generator to simulate how many games are needed to reach Legend.
+
+    :param win_rate: How likely the player is to win an individual game.
+    :param start_stars: How many stars the player begins with.
+    :param no_streaks_above_rank: The rank at which players no longer receive an extra star for a
+    win streak.
+    :param rank_floors: Once a player passes one of these ranks, they cannot fall back below it,
+    even with multiple losses.
+    :return: The number of simulated games played.
+    :rtype: int
+    """
     star_map = build_star_map()
     current_stars = start_stars
     star_rank_floors = [min(k for k, v in star_map.items() if v == floor) for floor in rank_floors]
